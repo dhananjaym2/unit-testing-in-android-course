d142b11f12ca62b88c85148c2c6b59eefeb99ed3
Dhananjay <dhananjaymohnot@gmail.com>
2020-08-30 17:31:33 +0530

    Write test cases for StringDuplicator for exercise 2.

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
@@ -1,5 +1,35 @@
 package com.techyourchance.unittestingfundamentals.exercise2;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class StringDuplicatorTest {
 
+  StringDuplicator SUT;
+
+  @Before
+  public void setUp() throws Exception {
+    SUT = new StringDuplicator();
+  }
+
+  @Test
+  public void duplicateString_emptyString_emptyStringReturned() {
+    String result = SUT.duplicate("");
+    assertThat(result, is(""));
+  }
+
+  @Test
+  public void duplicateString_singleCharacter_sameCharacterReturned() {
+    String result = SUT.duplicate("a");
+    assertThat(result, is("aa"));
+  }
+
+  @Test
+  public void duplicateString_longString_duplicateLongString() {
+    String result = SUT.duplicate("Example");
+    assertThat(result, is("ExampleExample"));
+  }
 }
